922a096ffd06e47e7174f28696a5242ab94f8089
Vung <vungdoxuankthd@gmail.com>
2022-08-04 17:17:07 +0700

    userReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import ReactMemo from "./components/ReactMemo/ReactMemo";
 import UseCallback from "./components/UseCallback/UseCallback";
 import UseEffect from "./components/UseEffect/UseEffect";
+import UseReducer from "./components/UseReducer/UseReducer";
 import UseState from "./components/UseState/UseState";
 
 function App() {
   return (
     <div className="App">
-      <ReactMemo />
+      <UseReducer />
     </div>
   );
 }
---
+++ src/components/UseReducer/UseReducer.js
@@ -0,0 +1,137 @@
+import React, { useReducer } from "react";
+import PropTypes from "prop-types";
+
+const reducer = (state, action) => {
+  switch (action) {
+    case "INCREASE":
+      return state + 1;
+    case "DECREASE":
+      return state - 1;
+    case "RESET":
+      return 0;
+    default:
+      return state;
+  }
+};
+
+const reducer2 = (state, action) => {
+  switch (action.type) {
+    case "SET_VALUE":
+      return action.data;
+    default:
+      return state;
+  }
+};
+
+const initState = {
+  loading: false,
+  data: [],
+};
+
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case "GET_USER_REQUEST":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "GET_USER_SUCCESS":
+      console.log("GET_USER_SUCCESS ở đây", action.data);
+      return {
+        ...state,
+        loading: false,
+        data: action.data,
+      };
+    case "GET_USER_ERROR":
+      return {
+        ...state,
+        data: [],
+        error: action.data,
+      };
+    default:
+      break;
+  }
+};
+
+function UseReducer(props) {
+  // useReducer trả về array với element đầu tiên là tên state, dispatch
+  const [count, dispatch] = useReducer(reducer, 0); //useReducer nhận vào hai tham số là reducer và giá trị khởi tạo
+  const [count2, dispatch2] = useReducer(reducer2, 0);
+  const [users, userDispatch] = useReducer(userReducer, initState);
+  console.log("users là", users);
+
+  const getUsers = () => {
+    console.log("ấn vào đây");
+    userDispatch({
+      type: "GET_USER_REQUEST",
+    });
+
+    setTimeout(() => {
+      fetch("https://reqres.in/api/users")
+        .then((res) => res.json())
+        .then((res) => {
+          userDispatch({
+            type: "GET_USER_SUCCESS",
+            data: res.data,
+          });
+        })
+        .catch((err) => {
+          userDispatch({
+            type: "GET_USER_ERROR",
+            data: err,
+          });
+        });
+    }, 2000);
+  };
+
+  return (
+    <>
+      <button onClick={getUsers}>GET USERS</button>
+      {users.loading ? <p>Loading...</p> : ""}
+      <ul>
+        {users.data.length > 0
+          ? users.data.map((user) => (
+              <li key={user.id}>
+                {user.last_name} {user.first_name}
+              </li>
+            ))
+          : ""}
+      </ul>
+      <h1>COUNT 1: {count}</h1>
+      <button onClick={() => dispatch("INCREASE")}>INCREASE</button>
+      <button>DECREASE</button>
+      <button>RESET</button>
+      <h1>COUNT 2: {count2}</h1>
+      <button
+        onClick={() =>
+          dispatch2({
+            type: "SET_VALUE",
+            data: 10,
+          })
+        }
+      >
+        SET VALUE
+      </button>
+    </>
+  );
+}
+
+UseReducer.propTypes = {};
+
+export default UseReducer;
+
+/** 
+ ACTION 'ADD_NEW_ITEM'
+
+ VIEW: Nhấn lên 1 button dispatch('ADD_NEW_ITEM')
+
+ REDUCERS chỉ là function nhận vào 2 tham số
+  (state, action)=>{
+    switch(action) {
+      case 'ADD_NEW_ITEM': 
+        state = state + 1
+      case 'ABC':
+        state = state...
+    }
+  }
+*/
